e635613d69919d570fa4e2541f9ea9ec2a599e61
Pedro Henrique Andrade de Almeida <pedro.henrique5661@gmail.com>
2026-08-26 01:36:26 +0000

    Codificação das Entidades

---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into categoria (nome, descricao) values ('Informática','Produtos de informática');
+insert into categoria (nome, descricao) values ('Livro','Biblioteca');
+insert into categoria (nome, descricao) values ('Computador','Vários computadores');
+insert into produto (nome, descricao, preco, estoque, categoria_id) values ('Código Limpo','Livro do autor Robert C. Martin', 100.00, 20, 2);
